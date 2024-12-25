9f88f9fdb684dc8f81de7cb3760363a1c1eeba45
Richard Anaya <richard.anaya@gmail.com>
2024-12-25 17:31:51 -0600

    fix

--- src/main.rs
+++ src/main.rs
@@ -185,8 +185,8 @@ async fn main() {
         .with_state(state_with_dir)
         .nest_service("/static", ServeDir::new("./static"));
 
-    // Run it with hyper on localhost:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Starting server... http://localhost:49999");
+    let addr = SocketAddr::from(([127, 0, 0, 1], 49999));
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("listening on {}", addr);
     axum::serve(listener, app).await.unwrap();
